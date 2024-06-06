ea386c1e420aa3103162ad8ffec50d5dcfa40554
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-06-06 21:50:35 +0300

    add GetAllAppointmentsByPatientQueryResponse, mappingProfiles

---
+++ Application/Features/Appointment/Queries/GetAllByPatient/GetAllAppointmentsByPatientQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Features.Appointment.Queries.GetAllByPatient
+{
+    public class GetAllAppointmentsByPatientQueryResponse
+    {
+        public int id { get; set; }
+        public string Status { get; set; }
+        public string FirstName { get; set; }
+        public string DoctorSpecialization { get; set; }
+        public DateTime AppointmentTime { get; set; }
+    }
+}
