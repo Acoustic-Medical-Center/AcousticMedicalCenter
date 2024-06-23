82e19ddf0a3d874b96c55751b0cc5d81fca52bf4
Batu YURT <asdasd018@hotmail.com>
2024-06-23 17:03:19 +0300

    Add Quartz library structure

--- Infrastructure/InfrastructureServiceRegistration.cs
+++ Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Services;
+using Infrastructure.Quartz;
 using Infrastructure.Services.EmailService;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,29 @@ namespace Infrastructure
         {
 
             services.AddScoped<IMailService, MailService>();
+
+
+            // Birden fazla quartz job yazılacaksa bu kısım farklı bir classa alınabilir
+            services.AddQuartz(configure =>
+            {
+                // Job anahtarı
+                var jobKey = new JobKey(nameof(CheckAppointmentTimeJob));
+
+                // Job ve trigger
+                configure
+                    .AddJob<CheckAppointmentTimeJob>(jobKey)
+                    .AddTrigger(trigger => trigger
+                        .ForJob(jobKey)
+                        .WithSimpleSchedule(schedule => schedule
+                            .WithIntervalInSeconds(30)
+                            .RepeatForever()));
+
+                // Microsoft Dependency Injection
+                configure.UseMicrosoftDependencyInjectionJobFactory();
+            });
+
+            // Quartz server ekle
+            services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
 
             return services;
         }
