e699aa5c560f52c787f799fdb5f39d73ee7ef5d3
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-06-27 15:00:09 +0300

    add dateFilter (AvailableAppointment)

--- Application/Features/Appointment/Queries/GetAllAvailableAppointments/GetAllAvailableAppointmentsQuery.cs
+++ Application/Features/Appointment/Queries/GetAllAvailableAppointments/GetAllAvailableAppointmentsQuery.cs
@@ -14,6 +14,7 @@ namespace Application.Features.Appointment.Queries.GetAllAvailableAppointments
     public class GetAllAvailableAppointmentsQuery : IRequest<List<GetAllAvailableAppointmentsQueryResponse>>
     {
         public int DoctorId { get; set; }
+        public DateTime? Date { get; set; }
 
         public class GetAllAppointmentsQueryHandler : IRequestHandler<GetAllAvailableAppointmentsQuery, List<GetAllAvailableAppointmentsQueryResponse>>
         {
@@ -29,14 +30,25 @@ namespace Application.Features.Appointment.Queries.GetAllAvailableAppointments
 
             public async Task<List<GetAllAvailableAppointmentsQueryResponse>> Handle(GetAllAvailableAppointmentsQuery request, CancellationToken cancellationToken)
             {
-                var availableAppointments = await _appointmentRepository.GetListAsync(predicate: d => d.DoctorId == request.DoctorId);
+                IEnumerable<Domain.Entities.Appointment> availableAppointments;
+                if (request.Date.HasValue)
+                {
+                    var dayStart = request.Date.Value.Date; // Günün başlangıcı (00:00)
+                    var dayEnd = dayStart.AddDays(1); // Günün sonu (00:00 sonraki gün)
 
+                    availableAppointments = await _appointmentRepository.GetListAsync(
+                        predicate: d => d.DoctorId == request.DoctorId &&
+                                        d.AppointmentTime >= dayStart &&
+                                        d.AppointmentTime < dayEnd);
+                }
+                else
+                {
+                    availableAppointments = await _appointmentRepository.GetListAsync(
+                        predicate: d => d.DoctorId == request.DoctorId);
+                }
 
                 var response = _mapper.Map<List<GetAllAvailableAppointmentsQueryResponse>>(availableAppointments);
-
                 return response;
-
-
 
             }
         }
