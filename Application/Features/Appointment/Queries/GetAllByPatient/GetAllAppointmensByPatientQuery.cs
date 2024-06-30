775b97183f7c8a0ce660bb47e029c1026cdfb7a1
Batu YURT <asdasd018@hotmail.com>
2024-06-30 18:11:55 +0300

    Add pagination to get all appointments by patient query

--- Application/Features/Appointment/Queries/GetAllByPatient/GetAllAppointmensByPatientQuery.cs
+++ Application/Features/Appointment/Queries/GetAllByPatient/GetAllAppointmensByPatientQuery.cs
@@ -15,6 +15,8 @@ namespace Application.Features.Appointment.Queries.GetAllByPatient
 {
     public class GetAllAppointmensByPatientQuery : IRequest<List<GetAllAppointmentsByPatientQueryResponse>>
     {
+        public int Page { get; set; } = 0;
+        public int PageSize { get; set; } = 0;
         public string DateFilter { get; set; } = "";
 
         public class GetAllAppointmensByPatientQueryHandler : IRequestHandler<GetAllAppointmensByPatientQuery, List<GetAllAppointmentsByPatientQueryResponse>>
@@ -46,15 +48,20 @@ namespace Application.Features.Appointment.Queries.GetAllByPatient
                     {
                         throw new BusinessException("Geçersiz hasta kimliği.");
                     }
-
-                    var appointments = await _appointmentsRepository.GetListAsync(
+                    var totalCount = _appointmentsRepository.GetList().Count();
+                    if (request.PageSize == 0)
+                    {
+                        request.PageSize = totalCount;
+                    }
+                    var appointments = _appointmentsRepository.GetList(
                         predicate: a => a.PatientId == patientId,
                         include: a => a
                             .Include(appt => appt.Doctor)
                             .ThenInclude(doc => doc.DoctorSpecialization)
                             .Include(appt => appt.Doctor)
                             .ThenInclude(d => d.User)
-                    );
+                    ).Skip((request.Page - 1) * request.PageSize)
+                     .Take(request.PageSize);
 
                     List<GetAllAppointmentsByPatientQueryResponse> response = new();
 
